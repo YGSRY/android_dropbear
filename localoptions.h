b004a9aa6a3f71a794feb8c76b84c7bacc4edf11
YGSRY <50430250+YGSRY@users.noreply.github.com>
2024-12-16 22:16:32 +0800

    Update localoptions.h

--- localoptions.h
+++ localoptions.h
@@ -5,24 +5,22 @@
  */
 
 // Disable server password auth as crypt() isn't available under Android
-#define DROPBEAR_SVR_PASSWORD_AUTH 0
+//#define DROPBEAR_SVR_PASSWORD_AUTH 0
 
 // Disable client password auth as getpass() isn't available under Android
-#define DROPBEAR_CLI_PASSWORD_AUTH 0
+//#define DROPBEAR_CLI_PASSWORD_AUTH 0
 
 // Speed up symmetrical ciphers and hashes at the expense of larger binaries
-#define DROPBEAR_SMALL_CODE 0
+//#define DROPBEAR_SMALL_CODE 0
 
 // Build all but the most verbose level of trace messages into the binaries
-#define DEBUG_TRACE 4
+//#define DEBUG_TRACE 4
 
 // Change the fallback list of shells to the non-standard Android shell path
-#define COMPAT_USER_SHELLS "/system/bin/sh"
+//#define COMPAT_USER_SHELLS "/system/bin/sh"
 
 //自添加
 
-#ifndef DROPBEAR_HOME
-#define DROPBEAR_HOME "/system/etc/dropbear"
-#endif
+
 
 
